ba21cb80c4b55c1ff5741c8522f499da46f55be9
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-04 04:57:50 +0530

    Create p6_secondlargest.java

---
+++ F2_Array/p6_secondlargest.java
@@ -0,0 +1,26 @@
+package F2_Array;
+
+class Larger {
+    public int secondLargest(int[] arr) {
+        int l = arr[0];
+        int res = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > l) {
+                res = l;
+                l = arr[i];
+
+            }
+        }
+        return res;
+    }
+}
+
+public class p6_secondlargest {
+
+    public static void main(String arg[]) {
+        Larger l = new Larger();
+        int arr[] = { 12, 32, 443, 545, 234 };
+        int s = l.secondLargest(arr);
+        System.out.println("Second Largest Number is:" + s);
+    }
+}
