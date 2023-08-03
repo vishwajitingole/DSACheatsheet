cbb8035fc715134569665506caaf408b37667ab0
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-04 00:50:49 +0530

    Create p1_array.java

---
+++ F2_Array/p1_array.java
@@ -0,0 +1,19 @@
+package F2_Array;
+
+import java.util.ArrayList;
+
+public class p1_array {
+    public static void main(String arg[]) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+
+            int arr2[] = new int[100];
+
+            // Dynamic array declaration
+            // ArrayList<Integer> dynamicArray = new ArrayList<Integer>();
+            // With an ArrayList, you can add or remove elements at runtime using methods
+            // like add() and remove(), making it a true dynamic array.
+        }
+    }
+}
