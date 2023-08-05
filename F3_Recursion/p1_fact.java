3c4e6e3bab90fc631b5279c9b64d21f8327f4087
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-06 05:03:11 +0530

    go

---
+++ F3_Recursion/p1_fact.java
@@ -0,0 +1,24 @@
+package F3_Recursion;
+
+import java.util.*;
+
+public class p1_fact {
+
+    public static int fact(int a) {
+        if (a == 1)
+            return 1;
+
+        return a * fact(a - 1);
+
+    }
+
+    public static void main(String arg[]) {
+
+        System.out.println("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        int f = fact(a);
+        System.out.println(f);
+    }
+}
