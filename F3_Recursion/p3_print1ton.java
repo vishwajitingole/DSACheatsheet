82de995a052f9026bb26b5351a47a90cf4598bec
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-06 05:14:29 +0530

    go

--- F3_Recursion/p3_print1ton.java
+++ F3_Recursion/p3_print1ton.java
@@ -4,19 +4,17 @@ import java.util.*;
 
 //Tail recursion are more  faster
 //the function call where the function call takes place at the end of the function
-
-//Program given below is a tail recursion function
-public class p2_printnto1 {
-    public static void print(int n) {
-        if (n == 0)
+public class p3_print1ton {
+    public static void print(int a) {
+        if (a == 0)
             return;
-        System.out.println(n);
-        print(n - 1);
+        print(a - 1);
+        System.out.println(a);
     }
 
     public static void main(String arg[]) {
+        System.out.println("Enter a number:");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number:");
         int n = sc.nextInt();
         print(n);
     }
