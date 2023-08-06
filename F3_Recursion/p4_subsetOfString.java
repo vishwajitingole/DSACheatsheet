82a9fcb1d1b1ccb855ade9cd104ab723aaf93825
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-06 06:07:36 +0530

    Update p4_subsetOfString.java

--- F3_Recursion/p4_subsetOfString.java
+++ F3_Recursion/p4_subsetOfString.java
@@ -13,33 +13,20 @@ class p4_subsetOfString {
 
         // base case
         if (index == n) {
+            System.out.println(curr);
             return;
         }
 
-        // First print current subset
-        System.out.println(curr);
+        print(str, index + 1, curr);
+        print(str, index + 1, curr + str.charAt(index));
 
-        // Try appending remaining characters
-        // to current subset
-        for (int i = index + 1; i < n; i++) {
-            curr += str.charAt(i);
-            print(str, i, curr);
-
-            // Once all subsets beginning with
-            // initial "curr" are printed, remove
-            // last character to consider a different
-            // prefix of subsets.
-            curr = curr.substring(0, curr.length() - 1);
-        }
     }
 
     // Driver code
     public static void main(String[] args) {
         String str = "abc";
-        int index = -1;
+        int index = 0; // Start index from 0
         String curr = "";
         print(str, index, curr);
     }
 }
-
-// This code is contributed by PrinciRaj1992
