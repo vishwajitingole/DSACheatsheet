f3a47f023676ba1cecafe3978dee72c9dd414be7
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-13 13:38:59 +0530

    Update p7_moveZerosToEnd.java

--- F2_Array/p7_moveZerosToEnd.java
+++ F2_Array/p7_moveZerosToEnd.java
@@ -4,23 +4,23 @@ public class p7_moveZerosToEnd {
     // This method moves all the non-zero elements to the beginning of the array and
     // fills the rest of the array with zeros
     public static void chalpeeche(int[] arr) {
-        int count = 0; // Initialize a variable 'count' to keep track of the non-zero elements
+        int zero = 0; // Initialize a variable 'count' to keep track of the non-zero elements
 
         // Loop through the array
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 // If the current element is non-zero, move it to the front of the array at the
-                // 'count' index
-                // Increment 'count' to prepare for the next non-zero element
-                arr[count++] = arr[i];
+                // 'zero' index
+                // Increment 'zero' to prepare for the next non-zero element
+                arr[zero++] = arr[i];
             }
         }
 
         // After the first loop, all non-zero elements are moved to the beginning of the
         // array
         // Now, we need to fill the remaining positions in the array with zeros
-        while (count < arr.length) {
-            arr[count++] = 0;
+        while (zero < arr.length) {
+            arr[zero++] = 0;
         }
     }
 
