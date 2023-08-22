f86ad92e49ea4defc7194ac9d675a5a796ae9957
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-22 10:16:34 +0530

    go

--- F3_Sorting/p4_mergeSort.java
+++ F3_Sorting/p4_mergeSort.java
@@ -9,7 +9,62 @@ package F3_Sorting;
 //Works on Divide and conquer algorithm
 
 public class p4_mergeSort {
-    public static void main(String arg[]) {
 
+    public static void mergeSort(int arr[], int l, int r) {
+
+        if (l < r) {
+            int m = (l + r) / 2; // Calculate the correct middle index
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+
+    }
+
+    public static void merge(int arr[], int l, int m, int r) {
+        int a = m - l + 1;
+        int b = r - m;
+        int left[] = new int[a];
+        int right[] = new int[b];
+        for (int i = 0; i < a; i++) {
+            left[i] = arr[l + i];
+        }
+        for (int j = 0; j < b; j++) {
+            right[j] = arr[m + 1 + j];
+        }
+        int i = 0, j = 0, k = l; // Start k from 'l' instead of '1'
+        while (i < a && j < b) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < a) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < b) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+
+    }
+
+    public static void main(String arg[]) {
+        int arr[] = { 1, 20, 3, 2, 8, 5, 19 };
+        int l = 0;
+        int r = arr.length - 1;
+        mergeSort(arr, l, r);
+
+        // Print the sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
