c8f24233bebd6bb2ae18f000b81b08e85c1eeea7
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-04-23 22:09:46 +0530

    BST

---
+++ F14_BST/BinarySearchTree.java
@@ -0,0 +1,73 @@
+package F14_BST;
+
+//Unlike other datastructures 
+
+//BST takes O(log n)
+//Time complexity in Search Insert Delete and Finding the closest value
+//Sorted traversal will take O(n)
+
+//Not cache friendly
+
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+public class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("Inorder traversal of the binary search tree:");
+        tree.inorder();
+    }
+}
+
---
+++ F14_BST/S1_SearchBST.java
@@ -0,0 +1,7 @@
+package F14_BST;
+
+public class S1_SearchBST {
+    public static void main(String[] args) {
+        
+    }
+}
