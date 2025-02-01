78053e2ff821de1052dfd42d74dcf8aa18a13a1a
Vishwajeet Vikas Ingole <72921903+vishwajitingole@users.noreply.github.com>
2025-02-01 20:04:08 +0530

    max freq

---
+++ F3_Array/p25_MaxFrequency.java
@@ -0,0 +1,11 @@
+package F3_Array;
+public class p25_MaxFrequency {
+    public static int totalFreq(int nums[]){
+        
+    }
+    
+    public static void main(String[] args) {
+        int arr[]={2,31,2,43,3,3};
+      System.out.print(  totalFreq(arr));
+    }
+}
