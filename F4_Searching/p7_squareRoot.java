cf017b1fe8a4ac03dc2676044edb17aa76ab307e
Vishwajeet Vikas Ingole <72921903+vishwajitingole@users.noreply.github.com>
2026-02-12 16:29:01 +0530

    Refactor sroot method for better performance
    Refactor square root function to handle edge cases and improve accuracy.

--- F4_Searching/p7_squareRoot.java
+++ F4_Searching/p7_squareRoot.java
@@ -1,23 +1,28 @@
 package F4_Searching;
 
 public class p7_squareRoot {
-    public static int sroot(int x) {
-        int low = 0;
-        int high = x;
-        int res = 0;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (x == mid * mid) {
-                return mid;
-            } else if (mid * mid > x) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-                res = mid;
+        public static int sroot(int x) {
+            if (x == 0 || x == 1) return x;
+        
+            int low = 1;
+            int high = x / 2;
+            int res = 0;
+        
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                long sq = (long) mid * mid;
+        
+                if (sq == x) {
+                    return mid;
+                } else if (sq > x) {
+                    high = mid - 1;
+                } else {
+                    res = mid;
+                    low = mid + 1;
+                }
             }
+            return res;
         }
-        return res;
-    }
 
     public static void main(String[] args) {
 
